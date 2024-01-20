7c3c99ad9c98141ec99dc0df0e4b9bd51e9e067d
Gustavo <paicofalero@gmail.com>
2024-01-20 14:38:20 -0800

    all view

--- view/Main/mainleftpanel.php
+++ view/Main/mainleftpanel.php
@@ -44,7 +44,7 @@
 									</a>
 								</li>
 								<li>
-									<a class="nav-link" href="">
+									<a class="nav-link" href="../Gestionresidentes/residentes.php">
 										Gestion de Residentes
 									</a>
 								</li>
